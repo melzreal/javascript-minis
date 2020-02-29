ae16ef699472d1660d3ec57c88e8b6dcdddf3844
melzreal <melissa.real@hotmail.com>
2020-02-29 19:08:05 +0000

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 document.getElementById('chuck-form').addEventListener('submit', function (e) {
     e.preventDefault();
-
     getChuckJokes();
 
 })
@@ -14,19 +13,16 @@ function getChuckJokes() {
 
     xhr.open('GET', `http://api.icndb.com/jokes/random/${nr}`, true);
 
-    console.log(`http://api.icndb.com/jokes/random/${nr}`)
     xhr.onload = function () {
         if (this.status === 200) {
-            let response = JSON.parse(this.responseText);
-
+            let resp = JSON.parse(this.responseText);
             let output = '';
 
-            if (response.type === 'success') {
-
-                response.value.forEach(function (jo) {
-                    output += `<li>${jo.joke}</li>\n<br>`
+            if (resp.type === 'success') {
+                resp.value.forEach(jo => {
+                    output += `<ul><li>${jo.joke}</li>\n<br></ul>`
                 })
-                console.log(output)
+
             } else {
                 output += '<ul><li>Something went wrong</li></ul>'
             }
